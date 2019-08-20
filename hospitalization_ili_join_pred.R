db4cfeaa39feadd8a733837c02edc4ec7882e0e4
graham gibson <gcgibson@umass.edu>
2019-08-20 13:39:40 -0600

    joint forecasting versus independent forecasting

--- hospitalization_ili_join_pred.R
+++ hospitalization_ili_join_pred.R
@@ -11,6 +11,22 @@ nat_data <- nat_data[nat_data$time %in% common_dates,]
 hosp_data <- hosp_data[hosp_data$time %in% common_dates,]
 
 
+# subset to training data
+
+nat_data_train <- nat_data$weighted_ili[1:200]
+
+# remap to [0,1]
+nat_data_train <- nat_data_train/(max(nat_data_train)+.00001)
+
+hosp_data_train <- hosp_data[hosp_data$age == 2,]$weeklyrate[1:200]
+# map to poisson
+hosp_data_train <- hosp_data_train*10
+
+
+jags.data = list("ILI"=nat_data_train,"N"=length(nat_data_train),
+                 "HOSP" =hosp_data_train)
+## joint estimation of hosp and wILI
+
 model.loc=("ss_model.txt")
 jagsscript = cat("
                  model {  
@@ -36,13 +52,11 @@ jagsscript = cat("
                  }  
                  ",file=model.loc)
 
-jags.data = list("ILI"=nat_data$weighted_ili/(max(nat_data$weighted_ili)+.000000001),"N"=length(nat_data$weighted_ili),
-                 "HOSP" = hosp_data[hosp_data$age == 2,]$weeklyrate*10)
+
 jags.params=c("TRUE_INFLUENZA_M1","phi_m1")
 mod_ss = jags(jags.data, parameters.to.save=jags.params, model.file=model.loc, n.chains = 3, 
 n.burnin=5000, n.thin=1, n.iter=10000, DIC=TRUE) 
-attach.jags(mod_ss)
-
+phi_m1 <- mean(mod_ss$BUGSoutput$sims.array[,,"phi_m1"])
 #plot(colMeans(TRUE_INFLUENZA))
 
 #plot(exp(colMeans(TRUE_INFLUENZA))/(1+exp(colMeans(TRUE_INFLUENZA))))
@@ -65,7 +79,6 @@ jagsscript = cat("
                  
                  v <-1
                  phi_m2 ~ dnorm(0,1);
-                 
                  TRUE_INFLUENZA_M2[1] <- mu;
                  beta_mean[1] <- exp(TRUE_INFLUENZA_M2[1])/(1+exp(TRUE_INFLUENZA_M2[1]))
                  ILI[1] ~ dbeta( v*beta_mean[1],(1-beta_mean[1])*v );
@@ -79,12 +92,10 @@ jagsscript = cat("
                  }  
                  ",file=model.loc)
 
-jags.data = list("ILI"=nat_data$weighted_ili/(max(nat_data$weighted_ili)+.000000001),"N"=length(nat_data$weighted_ili))
 jags.params=c("TRUE_INFLUENZA_M2","phi_m2")
 mod_ss_independent_wILI = jags(jags.data, parameters.to.save=jags.params, model.file=model.loc, n.chains = 3, 
               n.burnin=5000, n.thin=1, n.iter=10000, DIC=TRUE) 
-attach.jags(mod_ss_independent_wILI)
-
+phi_m2 <- mean(mod_ss_independent_wILI$BUGSoutput$sims.array[,,"phi_m2"])
 
 
 ### HOSP Independent
@@ -99,7 +110,7 @@ jagsscript = cat("
                  
                  v <-1
                  phi_m3 ~ dnorm(0,1);
-                 
+                 TRUE_INFLUENZA_M3[1] <- mu
                  HOSP[1] ~ dpois(exp(TRUE_INFLUENZA_M3[1]));
                  
                 for(i in 2:N) {
@@ -110,19 +121,43 @@ jagsscript = cat("
                  }  
                  ",file=model.loc)
 
-jags.data = list("ILI"=nat_data$weighted_ili/(max(nat_data$weighted_ili)+.000000001),"N"=length(nat_data$weighted_ili))
 jags.params=c("TRUE_INFLUENZA_M3","phi_m3")
 mod_ss_independent_hosp = jags(jags.data, parameters.to.save=jags.params, model.file=model.loc, n.chains = 3, 
               n.burnin=5000, n.thin=1, n.iter=10000, DIC=TRUE) 
-attach.jags(mod_ss_independent_hosp)
-
+phi_m3 <- mean(mod_ss_independent_hosp$BUGSoutput$sims.array[,,"phi_m3"])
 
 
 ### AR(1) Evaluation
 
+nat_data_test <- nat_data$weighted_ili[201:276]
+nat_data_test <- nat_data_test/(max(nat_data_test)+.00001)
+hosp_data_test <- hosp_data[hosp_data$age == 2,]$weeklyrate[201:276]
+hosp_data_test <- hosp_data_test*10
 
+mse_m1_ili <- c()
+mse_m1_hosp <- c()
+mse_m2 <- c()
+mse_m3 <- c()
 
+for (i in 1:(length(nat_data_test)-1)){
+  ili_hat_under_joint <- phi_m1*nat_data_test[i]
+  ili_hat_under_indp <- phi_m2*nat_data_test[i]
+  
+  hosp_hat_under_joint <- phi_m1*hosp_data_test[i]
+  hosp_hat_under_ind   <- phi_m3*hosp_data_test[i]
+  
+  truth_ili <- nat_data_test[i+1]
+  truth_hosp <- hosp_data_test[i+1]
+  
+  mse_m1_ili <- c(mse_m1_ili,(ili_hat_under_joint-truth_ili)^2)
+  mse_m1_hosp <- c(mse_m1_hosp,(hosp_hat_under_joint-truth_hosp)^2)
+  mse_m2 <- c(mse_m2,(ili_hat_under_indp-truth_ili)^2)
+  mse_m3 <- c(mse_m3,(hosp_hat_under_ind-truth_hosp)^2)
+}
 
+mean(mse_m1_ili)
+mean(mse_m2)
 
-
+mean(mse_m1_hosp)
+mean(mse_m3)
 
